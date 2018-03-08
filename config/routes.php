456c663ee12f9cf99741629fca7de1385ed3f6ad
Alex Markessinis <markea125@gmail.com>
2018-03-08 13:57:15 -0500

    updated index view

--- config/routes.php
+++ config/routes.php
@@ -11,4 +11,18 @@
 */
 
 return [
+    'purge_all' => [
+        'title' => 'Buster Purge All',
+        'capability' => 'read',
+        'route' => [
+            'get' => 'DashboardController@bustAll'
+        ]
+    ],
+    'purge_page' => [
+        'title' => 'Buster Purge Page',
+        'capability' => 'read',
+        'route' => [
+            'post' => 'DashboardController@bustPage'
+        ]
+    ]
 ];
